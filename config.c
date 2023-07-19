970aa779fab3e8c5abeb2fafbda6403b6330b44d
Hammas <dev.hammas@gmail.com>
2023-07-20 02:58:09 +0500

    add scripts

--- config.c
+++ config.c
@@ -4,16 +4,12 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
-    {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
-    {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    { "sb-nettraf",    1,      5 },
+    { "sb-internet",   5,      6 },
+	  { "sb-cpu",		    10,      7 },
+    { "sb-memory",     1,      8 },
+    { "sb-volume",     0,      9 },
+    { "sb-datetime",  1,     10 },
 };
 
 const unsigned short blockCount = LEN(blocks);
